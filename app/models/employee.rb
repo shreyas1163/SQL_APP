897b4ae9d6ed5ad62eca867b5044543835cbf1c1
shreyas1163 <30569773+shreyas1163@users.noreply.github.com>
2017-09-18 18:20:49 +0530

    did the changes

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -8,15 +8,12 @@ class Employee < ActiveRecord::Base
 	def self.records_create
     employee_result=Array.new
     1000000.times do |record|
-      person_name = ('a'..'z').to_a.shuffle.first(8).join
+      person_name = ('a'..'z').to_a.shuffle.first(5).join.Titelize
       salary = rand(10000..100000)
       manager_id = rand(100..400)
-      if(employee_result.count==0)
-        employee_result.push({:name=>person_name,:salary=> salary,:manager_id=> manager_id})
-      else
-        employee_result.push({:name=>person_name,:salary=> salary,:manager_id=> manager_id})
+             employee_result.push({:name=>person_name,:salary=> salary,:manager_id=> manager_id})
       end
-    end
+   
 
     Employee.bulk_insert do |worker|
       employee_result.each do |attrs|
@@ -41,11 +38,9 @@ class Employee < ActiveRecord::Base
 
 	#--------this is used for truncating all tables -----
 	def self.records_truncate
-		ActiveRecord::Base.establish_connection
-		ActiveRecord::Base.connection.tables.each do |table|
-			next if table == 'schema_migrations'
-			table.classify.constantize.connection.execute("SET FOREIGN_KEY_CHECKS = 0")
-			table.classify.constantize.connection.execute("TRUNCATE #{table}")
+		['employees','employee_cities'].each do |table|
+  			table.classify.constantize.connection.execute("SET FOREIGN_KEY_CHECKS = 0")
+  			table.classify.constantize.connection.execute("TRUNCATE #{table}")
 			table.classify.constantize.connection.execute("SET FOREIGN_KEY_CHECKS = 1")
 		end
 	end
